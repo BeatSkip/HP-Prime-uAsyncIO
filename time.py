eb3f4e1285adab0fc0e1c48f791a3a6715be5f9c
Oscar Spierings <oscar.spierings@optics11.com>
2022-01-31 12:52:08 +0100

    first partially working test

---
+++ time.py
@@ -0,0 +1,11 @@
+from hpprime import *
+from math import *
+
+def ticks_ms():
+    return eval("ticks()")
+
+def ticks_diff(ticks1,ticks2):
+    return ticks1-ticks2
+
+def ticks_add(ticks1,ticks2):
+    return ticks1 + ticks2
